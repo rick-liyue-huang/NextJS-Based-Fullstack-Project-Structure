91de1c513a3b5212af76da9c2607fc8e0b1e49af
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-07-09 20:22:28 +1000

    feat: create card by stroybook

---
+++ components/cards/cat/CatCard.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import CatCard, { ICatCardProps } from './CatCard';
+import { mockCatCardProps } from './CatCard.mocks';
+
+export default {
+  title: 'cards/CatCard',
+  component: CatCard,
+  argTypes: {},
+} as ComponentMeta<typeof CatCard>;
+
+const Template: ComponentStory<typeof CatCard> = (args) => (
+  <CatCard {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockCatCardProps.base,
+} as ICatCardProps;
